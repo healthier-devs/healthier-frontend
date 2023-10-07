ecb03eb6ebc01db1e6891107a1946dfb245041a0
moonki <moonki0623@naver.com>
2023-10-07 17:10:54 +0900

    feat: 챌린지 상세 하단 안내패널 구현

---
+++ src/pages/challenge-detail/challenge-notification/index.style.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: ${({ theme }) => theme.color.grey_800};
+  padding: 3.2rem 2.4rem 8rem;
+  .mainTitle {
+    font-size: 1.6rem;
+    line-height: 160%;
+    font-weight: 200;
+    color: ${({ theme }) => theme.color.grey_300};
+    margin-bottom: 1.2rem;
+  }
+  .subTitle {
+    font-size: 1.3rem;
+    font-weight: 100;
+    list-style: disc;
+    list-style-position: inside;
+    line-height: 170%;
+    color: ${({ theme }) => theme.color.grey_400};
+  }
+`;
