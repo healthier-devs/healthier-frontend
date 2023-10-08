4e151f0cc0ebf56cd26796404fb2aae6c3c7b54d
jin <53258214+kauthenticity@users.noreply.github.com>
2023-10-08 19:54:52 +0900

    feat: 맞춤 증상 탭 개발

---
+++ src/pages/main/symptoms/index.style.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const BannerContainer = styled.div`
+  position: relative;
+
+  width: 100%;
+  height: 140px;
+
+  border-radius: 8px;
+
+  background: ${({ theme }) => theme.color.blue};
+`;
+
+export const TextContainer = styled.div`
+  position: absolute;
+  left: 16px;
+  top: 20px;
+`;
+
+export const TitleContainer = styled.div`
+  padding: 4px 6px;
+
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.color.blue_400};
+  background: ${({ theme }) => theme.color.blue_500};
+
+  margin-bottom: 18px;
+
+  .title {
+    color: ${({ theme }) => theme.color.grey_100};
+
+    font-size: 2rem;
+    font-weight: 300;
+    letter-spacing: -0.5px;
+  }
+`;
+
+export const Description = styled.p`
+  color: #fff;
+  font-size: 1.4rem;
+  font-weight: 200;
+  line-height: 150%;
+
+  white-space: pre-line;
+`;
+
+export const Illustration = styled.img`
+  position: absolute;
+  right: 0;
+
+  height: 140px;
+`;
