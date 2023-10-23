e6edff609fb1bb891cf902735a16c6a787d6b3b6
jin <53258214+kauthenticity@users.noreply.github.com>
2023-10-24 00:08:56 +0900

    feat: 프로필 수정 화면 마크업

--- src/pages/account/edit/index.tsx
+++ src/pages/account/edit/index.tsx
@@ -1,5 +1,36 @@
+import { Link } from "react-router-dom";
+import { ChevronRightIcon } from "src/assets/icons/ChevronRightIcon";
+import Divider from "src/components/divider";
+import FlexBox from "src/components/flexBox";
+import theme from "src/lib/theme";
+import * as Lib from "../../signUp/lib";
+import { Box, Typography } from "../settings/index.style";
+
 function Edit() {
-  return <div> 수정</div>;
+  return (
+    <div>
+      <Box padding="3.2rem 2.4rem 0">
+        <Box mb="4rem">
+          <Lib.TextField id="account-id" label="이름" value="정설아" disabled />
+        </Box>
+        <Box mb="4rem">
+          <Lib.TextField id="account-phone-number" label="전화번호" value="010-1234-5678" disabled />
+        </Box>
+        <Box>
+          <Lib.TextField id="account-email" label="이메일" value="healthier@email.com" disabled />
+        </Box>
+      </Box>
+      <Divider />
+      <Box padding="2rem 2.4rem 0">
+        <Link to="/account/edit/password">
+          <FlexBox alignItems="center" justifyContent="space-between" className="click">
+            <Typography className="title-3">비밀번호 수정하기</Typography>
+            <ChevronRightIcon stroke={theme.color.grey_500} strokeWidth={2} width={24} height={24} />
+          </FlexBox>
+        </Link>
+      </Box>
+    </div>
+  );
 }
 
 export default Edit;
