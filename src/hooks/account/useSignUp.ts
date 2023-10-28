38684a0da8514223b3ef7312f6cb7d2e48b813de
jin <53258214+kauthenticity@users.noreply.github.com>
2023-10-28 22:48:14 +0900

    fix: 회원가입 후 로그인 커스텀 훅 호출 시점 변경

--- src/hooks/account/useSignUp.ts
+++ src/hooks/account/useSignUp.ts
@@ -1,20 +1,21 @@
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { signup } from "src/api/account/service";
+import { useLogin } from "./useLogin";
 import type { ISignUpRequest } from "src/interfaces/account";
 
 export const useSignUp = () => {
   const navigate = useNavigate();
+  const { login } = useLogin();
   const { mutate: signUp } = useMutation({
     mutationFn: (body: ISignUpRequest) => signup(body),
     onSuccess(data, variables) {
       if (data.success) {
-        navigate("/signup/complete", {
-          state: {
-            username: variables.username,
-            password: variables.password,
-          },
+        login({
+          username: variables.username,
+          password: variables.password,
         });
+        navigate("/signup/complete");
 
         return;
       }
