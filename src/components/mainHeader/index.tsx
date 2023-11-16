b3abcbf73ebb42c3487c3310469b89788e39fa1f
jin <53258214+kauthenticity@users.noreply.github.com>
2023-11-16 23:18:47 +0900

    feat: 메인 헤더 기본 핸들러 추가

--- src/components/mainHeader/index.tsx
+++ src/components/mainHeader/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import imageUrl from "src/data/image_url";
 import HeaderContainer from "../headerContainer";
 import { Container } from "./index.style";
@@ -7,10 +8,19 @@ interface IMainHeader {
 }
 
 const MainHeader = ({ onClickLogo }: IMainHeader) => {
+  const navigate = useNavigate();
+
   return (
     <HeaderContainer>
       <Container>
-        <div onClick={onClickLogo}>
+        <div
+          onClick={
+            onClickLogo ||
+            function () {
+              navigate("/");
+            }
+          }
+        >
           <img className="logo" alt="logo" src={imageUrl.logo} height={24} />
         </div>
       </Container>
