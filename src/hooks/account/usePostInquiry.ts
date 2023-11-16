58a6edee49b0de5f7c38f749becfe3a3032e7719
jin <53258214+kauthenticity@users.noreply.github.com>
2023-11-16 22:57:59 +0900

    feat: 문의하기 등록 완료 후 처리

--- src/hooks/account/usePostInquiry.ts
+++ src/hooks/account/usePostInquiry.ts
@@ -1,13 +1,21 @@
 import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import { accountFetcher } from "src/api/account/fetcher";
 import { queryKeys } from "src/api/queryKeys";
 import { IInquiry } from "src/interfaces/account";
 
 export const usePostInquiry = () => {
+  const navigate = useNavigate();
   const { mutate: postInquiry } = useMutation({
     mutationFn: (inquiry: IInquiry) => accountFetcher.postInquiry(inquiry),
     onSuccess() {
       queryKeys.INQUIRY;
+      alert("문의 등록이 완료됐습니다");
+      navigate("/account");
+    },
+    onError() {
+      alert("에러가 발생했습니다");
+      navigate("/account");
     },
   });
 
