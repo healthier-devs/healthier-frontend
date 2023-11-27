3e52f44dde09b0378d451e1ec0f92f96accaed21
jin <53258214+kauthenticity@users.noreply.github.com>
2023-11-27 15:44:22 +0900

    fix: 위치정보 약관 제거

--- src/pages/account/settings/terms/index.tsx
+++ src/pages/account/settings/terms/index.tsx
@@ -21,19 +21,10 @@ function Terms() {
         </Link>
       </Styled.Box>
 
-      <Styled.Box mb="1.8rem">
+      <Styled.Box>
         <Link to={NECESSARY_AGREEMENTS[1].url} target="_blank">
           <FlexBox alignItems="center" justifyContent="space-between" className="click">
             <Styled.Typography className="title-2">개인정보 처리방침</Styled.Typography>
-            <ChevronRightIcon stroke={theme.color.grey_500} strokeWidth={2} width={24} height={24} />
-          </FlexBox>
-        </Link>
-      </Styled.Box>
-
-      <Styled.Box>
-        <Link to={OPTIONAL_AGREEMENTS[0].url} target="_blank">
-          <FlexBox alignItems="center" justifyContent="space-between" className="click">
-            <Styled.Typography className="title-2">위치정보 이용 약관</Styled.Typography>
             <ChevronRightIcon stroke={theme.color.grey_500} strokeWidth={2} width={24} height={24} />
           </FlexBox>
         </Link>
